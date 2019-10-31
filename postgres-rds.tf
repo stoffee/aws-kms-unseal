508917f5b7b36d070e19f26d4bcb697fc89b5688
stoffee <chris@roadkill.org>
2019-10-31 16:21:11 -0700

    added postgres

---
+++ postgres-rds.tf
@@ -0,0 +1,10 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "11.5"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "stoffee"
+  password             = "!4me2know!"
+}
