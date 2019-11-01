8857e6e1b9e264a299be7b99396d5942e93211dd
stoffee <chris@roadkill.org>
2019-11-01 14:32:11 -0700

    added sg and db subnet

--- postgres-rds.tf
+++ postgres-rds.tf
@@ -1,3 +1,12 @@
+resource "aws_db_subnet_group" "postgres" {
+  name       = "main"
+  subnet_ids = ["${aws_subnet.public_subnet.id}"]
+
+  tags = {
+    Name = "Postgres DB subnet group"
+  }
+}
+
 resource "aws_db_instance" "default" {
   allocated_storage    = 20
   storage_type         = "gp2"
@@ -9,6 +18,6 @@ resource "aws_db_instance" "default" {
   password             = "!4me2know!"
   publicly_accessible  = true
   skip_final_snapshot  = true
- # vpc_security_group_ids = [aws_security_group.vault.id]
- # db_subnet_group_name = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.vault.id]
+  db_subnet_group_name = aws_db_subnet_group.postgres.subnet_ids
 }
