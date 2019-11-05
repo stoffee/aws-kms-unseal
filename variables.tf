bb67e7a6389ae8ac28437c57cc353b5ec45aa38d
stoffee <chris@roadkill.org>
2019-11-04 17:12:00 -0800

    postgres_cidr

--- variables.tf
+++ variables.tf
@@ -20,12 +20,11 @@ variable "vpc_cidr" {
   default     = "192.168.100.0/24"
 }
 
-# removing for postgres branch
-#variable "postgres_cidr" {
-#  type        = string
-#  description = "CIDR of the VPC"
-#  default     = "10.0.1.0/24"
-#}
+variable "postgres_cidr" {
+  type        = string
+  description = "CIDR of the VPC"
+  default     = "10.0.1.0/24"
+}
 
 variable "db_address" {
   description = "address of the DB server"
