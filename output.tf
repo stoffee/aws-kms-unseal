b2201831b6731eb2084d465f070662ebde7917ef
stoffee <chris@roadkill.org>
2019-11-10 21:57:23 -0800

    moved vault backend to postgres

--- output.tf
+++ output.tf
@@ -8,13 +8,6 @@ output "vault_db_instance_address" {
   value       = "${aws_db_instance.vault.address}"
 }
 
-output "connections" {
-  value = <<VAULT
-Connect to Vault via SSH   ssh -i private.key ubuntu@${aws_instance.vault[0].public_ip}
-Vault web interface  http://${aws_instance.vault[0].public_ip}:8200/ui
-NGINX web interface  https://${aws_instance.vault[0].public_ip}
-VAULT
-
 }
 output "connections" {
   value = <<VAULT
@@ -22,5 +15,4 @@ Connect to Vault via SSH   ssh -i private.key ubuntu@${aws_instance.vault[0].pub
 Vault web interface  http://${aws_instance.vault[0].public_ip}:8200/ui
 NGINX web interface  https://${aws_instance.vault[0].public_ip}
 VAULT
-
 }
