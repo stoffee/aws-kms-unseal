e34eac8eae9a21849df7cada279b7f48cbc7b392
stoffee <chris@roadkill.org>
2020-03-31 14:30:09 -0700

    added jump instance

--- ssh-instance.tf
+++ ssh-instance.tf
@@ -1,18 +1,3 @@
-data "aws_ami" "ubuntu" {
-  most_recent = "true"
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "ssh" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.medium"
