7d43438cc9cc8349f6ebcdd8961f338c18ed4d8d
stoffee <chris@roadkill.org>
2020-03-31 14:49:05 -0700

    added jump instance

--- output.tf
+++ output.tf
@@ -12,6 +12,7 @@ output "connections" {
   value = <<VAULT
 Connect to Vault via SSH   ssh -i private.key ubuntu@${aws_instance.vault[0].public_ip}
 Vault web interface  http://${aws_instance.vault[0].public_ip}:8200/ui
+SSH Host             ssh -i ssh key ubuntu@${aws_instance.ssh[0].public_ip}
 NGINX web interface  https://${aws_instance.vault[0].public_ip}
 VAULT
 }
